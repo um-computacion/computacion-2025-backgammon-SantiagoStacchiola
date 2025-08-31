e9396a69fa8535da6a666cf6339e867e54bea8be
unknown <s.stacchiola@alumno.um.edu.ar>
2025-08-31 09:44:42 -0300

    desarrollo de la clase player

--- core/player.py
+++ core/player.py
@@ -16,19 +16,24 @@ class Player:
         return self.__total_fichas__
 
     def enviar_a_barra(self):
-        ...
+        # Agrega una ficha a la barra
+        self.__barra__.append(self.__color__)
     
     def sacar_de_barra(self):
-        ...
+        # Saca una ficha de la barra (si existe)
+        if self.__barra__:
+            return self.__barra__.pop()
+        return None
     
     def fichas_en_barra(self):
-        ...
+        return len(self.__barra__)
 
-    def sacar_del_tablero(self)
-        ...
+    def sacar_del_tablero(self):
+        # Agrega una ficha a la zona de fichas fuera
+        self.__fuera__.append(self.__color__)
 
     def fichas_fuera(self):
-        ...
+        return len(self.__fuera__)
 
     def fichas_restantes(self):
         # Cantidad de fichas que aún tiene en juego
