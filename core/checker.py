98f59f51e2ab3a98a43162a365e5d9e800d19e4b
unknown <s.stacchiola@alumno.um.edu.ar>
2025-09-15 21:07:55 -0300

    agregar funciones a checker

--- core/checker.py
+++ core/checker.py
@@ -3,4 +3,12 @@ class Ficha:
         if color not in ["blanca", "negra"]:
             raise ValueError("Color inválido: debe ser 'blanca' o 'negra'")
         self.__color__ = color
-        self.__posicion__ = posicion 
+        self.__posicion__ = posicion
+
+    def obtener_color(self):
+        return self.__color__
+
+    def obtener_posicion(self):
+        return self.__posicion__
+    
+    
